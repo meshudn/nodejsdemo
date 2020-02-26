54efdaa816eaa45c25fdab82a76530af22a42919
Meshu Deb Nath <meshu.uiu@gmail.com>
2020-02-26 21:44:40 +0100

    readfile example

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world guru from node js");
+const fs = require('fs');
+
+fs.writeFile("example.txt","this is text", (err)=> {
+    if(err){
+        console.log(err);
+    }else{
+        console.log("file successfully created");
+        fs.readFile("example.txt","utf8",(err,data)=>{
+            console.log(data);
+        });
+    }
+});
